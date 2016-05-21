248514406c3fed1cf10b245a1d50248b7dc1bc84
Sawyer <sawyer_007@outlook.com>
2016-05-21 09:11:09 +0200

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://ruby.taobao.org'
 
 gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'faker', '~> 1.6', '>= 1.6.3'
+gem 'will_paginate', '~> 3.1'
+gem 'bootstrap-will_paginate', '~> 0.0.10'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page], :per_page => 20)
+  end
+
 	def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], :per_page => 10)
   end
   
   def new
@@ -10,6 +17,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   def update
@@ -39,15 +52,12 @@ class UsersController < ApplicationController
                                  :password_confirmation)
     end
 
-    def signed_in_user
-      unless signed_in?
-        flash[:warning] = "Please sign in."
-        redirect_to signin_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
